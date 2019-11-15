7045a9702f6368d5c92de9048084d4fe5eefbd40
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-11-15 21:09:53 +0900

    update

--- auto-aircon.py
+++ auto-aircon.py
@@ -18,14 +18,16 @@ DEF_TOPIC = 'env1/temp'
 DEF_PIN = 22
 
 
-class AutoAirconHeater:
+class AutoAircon:
     D_SEC_MAX = 3600  # 1 hour
 
-    DEF_KP = 0.2
-    DEF_KI = 0.0001
+    DEF_KP = 0.8
+    DEF_KI = 0.001
     DEF_KD = 50
 
     REMOCON_TEMP_INIT = 25
+
+    HIST_MAX_SEC = 60
 
     def __init__(self, dev_name, button_header, target_temp, token, topic, pin,
                  debug=False):
@@ -47,16 +49,32 @@ class AutoAirconHeater:
         self._prev_ts_msec = 0
         self._i_temp = 0
 
+        self._temp_hist = []  # history [[ts1, temp1], [ts2, temp2], ..]
+
         self._kp = self.DEF_KP
         self._ki = self.DEF_KI
         self._kd = self.DEF_KD
 
-        self._remocon_temp = self.REMOCON_TEMP_INIT
+        self._remocon_temp = round(self._target_temp)
 
         self._temp_subscriber = BbtTempSubscriber(self._token, self._topic,
                                                   debug=False)
 
         self._aircon = Aircon(self._dev_name, self._pin, debug=self._debug)
+
+    def add_hist(self, ts, temp):
+        self._logger.debug('ts=%d sec, temp=%f', ts, temp)
+
+        self._temp_hist.append({'ts': ts, 'temp': temp})
+        d_sec = ts - self._temp_hist[0]['ts']
+        self._logger.debug('d_sec=%.2f, temp_hist=%s', d_sec, self._temp_hist)
+        while d_sec > self.HIST_MAX_SEC:
+            self._temp_hist.pop(0)
+            d_sec = ts - self._temp_hist[0]['ts']
+            self._logger.debug('d_sec=%.2f, temp_hist=%s',
+                               d_sec, self._temp_hist)
+
+        return self._temp_hist
 
     def run(self):
         self._logger.debug('')
@@ -73,15 +91,18 @@ class AutoAirconHeater:
 
         while True:
             ts_msec, cur_temp = self._temp_subscriber.get_temp()
-            self._logger.debug('ts_msec=%d, cur_temp=%f', ts_msec, cur_temp)
+            ts = ts_msec / 1000
+            self._logger.debug('ts=%f, cur_temp=%f', ts_msec, cur_temp)
             if ts_msec == 0:
                 self._logger.warning('ignore')
                 continue
 
+            self.add_hist(ts, cur_temp)
+
             date_str = self._temp_subscriber.ts2datestr(ts_msec)
             self._logger.debug('date_str=%s', date_str)
 
-            remocon_temp = self.calc_remocon_temp(ts_msec, cur_temp)
+            remocon_temp = self.calc_remocon_temp(self._temp_hist)
             self._logger.debug('remocon_temp=%d', remocon_temp)
             if remocon_temp < 0:
                 self._logger.warning('ignored')
@@ -99,59 +120,54 @@ class AutoAirconHeater:
 
     def end(self):
         self._logger.debug('')
-        self._aircon.send('off')
+        #self._aircon.send('off')
         self._aircon.end()
         self._temp_subscriber.end()
 
-    def calc_remocon_temp(self, ts_msec, cur_temp):
-        self._logger.debug('ts_msec=%d, cur_temp=%f', ts_msec, cur_temp)
+    def calc_remocon_temp(self, temp_hist):
+        self._logger.debug('temp_hist=%s', temp_hist)
 
-        if self._prev_ts_msec == 0:
-            self._logger.debug('_prev_ts_msec=%d: ignored', self._prev_ts_msec)
-            self._prev_temp = cur_temp
-            self._prev_ts_msec = ts_msec
-            self._i_temp = 0
+        if len(temp_hist) < 2:
             return -1
 
-        d_sec = (ts_msec - self._prev_ts_msec) / 1000  # sec
-        self._logger.debug('d_sec=%d', d_sec)
-
-        if d_sec > self.D_SEC_MAX:
-            self._logger.warning('d_sec > %d', self.D_SEC_MAX)
-            self._prev_temp = cur_temp
-            self._prev_ts_msec = ts_msec
-            self._i_temp = 0
-            return -1
+        cur_ts     = temp_hist[-1]['ts']
+        cur_temp   = temp_hist[-1]['temp']
+        prev_ts    = temp_hist[-2]['ts']
+        prev_temp  = temp_hist[-2]['temp']
+        first_ts   = temp_hist[0]['ts']
+        first_temp = temp_hist[0]['temp']
 
         #
         # calc PID
         #
-        p_temp = cur_temp - self._target_temp
-        self._logger.debug('p_temp=%f', p_temp)
+        sum_temp = 0
+        for h in temp_hist:
+            sum_temp += h['temp']
+        ave_temp = sum_temp / len(temp_hist)
+        self._logger.debug('ave_temp=%f', ave_temp)
+            
+        p_temp = ave_temp - self._target_temp
+        i_temp = self._i_temp \
+            + ((cur_temp + prev_temp) * (cur_ts - prev_ts) / 2) \
+            - self._target_temp * (cur_ts - prev_ts)
+        d_temp = (cur_temp - first_temp) / (cur_ts - first_ts)
+        self._logger.debug('(p,i,d)_temp=(%f, %f, %f)', p_temp, i_temp, d_temp)
 
-        i_temp = self._i_temp + (p_temp * d_sec)
-        self._logger.debug('i_temp=%f (_i_temp=%f)', i_temp, self._i_temp)
-
-        d_temp = (cur_temp - self._prev_temp) / d_sec
-        self._logger.debug('d_temp=%f', d_temp)
-
-        self._prev_temp = cur_temp
-        self._prev_ts_msec = ts_msec
         self._i_temp = i_temp
 
-        work_p = self._kp * p_temp
-        work_i = self._ki * i_temp
-        work_d = self._kd * d_temp
-        self._logger.debug('work_p=%f, work_i=%f, work_d=%f',
-                           work_p, work_i, work_d)
+        p_work = self._kp * p_temp
+        i_work = self._ki * i_temp
+        d_work = self._kd * d_temp
+        self._logger.debug('(p,i,d)_work=(%f, %f, %f)', p_work, i_work, d_work)
 
-        pid = work_p + work_i + work_d
+        pid = p_work + i_work + d_work
         self._logger.debug('pid=%f', pid)
 
         #
         # calc remocon_temp
         #
-        remocon_temp = round(self._remocon_temp - pid)
+        #remocon_temp = round(self._remocon_temp - pid)
+        remocon_temp = round(self._target_temp - pid)
         self._logger.debug('remocon_temp=%d', remocon_temp)
 
         if remocon_temp == self._remocon_temp:
@@ -200,12 +216,12 @@ class App:
         self._topic = topic
         self._pin = pin
 
-        self._auto_remocon = AutoAirconHeater(self._dev_name,
-                                              self._button_header,
-                                              self._target_temp,
-                                              self._token, self._topic,
-                                              self._pin,
-                                              debug=self._debug)
+        self._auto_remocon = AutoAircon(self._dev_name,
+                                        self._button_header,
+                                        self._target_temp,
+                                        self._token, self._topic,
+                                        self._pin,
+                                        debug=self._debug)
 
     def main(self):
         self._logger.debug('')
