259750dcd8e616fb047e8df3443661a2b4aca234
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-11-28 01:50:29 +0900

    update

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@ COUNT=$1
 
 while [ $COUNT -gt 0 ]; do
     echo $COUNT
-    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 ir1 lamp &
+    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 irsend lamp &
     sleep 1
-    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 ir1 lamp on &
+    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 irsend lamp on &
     sleep 1
-    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 ir1 lamp off &
+    ./TcpCmdClient.py -s 192.168.0.220 -p 12399 irsend lamp off &
     sleep 1
     COUNT=`expr $COUNT - 1`
 done
