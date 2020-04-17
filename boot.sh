0f068fa9612266034dfbf39d88294a74e858a1de
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-04-17 16:12:02 +0900

    update

--- boot.sh
+++ boot.sh
@@ -6,33 +6,36 @@ MYNAME=`basename $0`
 
 DATE_FMT="%Y/%m/%d %H:%M:%S"
 ts_echo () {
-    echo `date +"${DATE_FMT}"` $*
+    echo "`date +"${DATE_FMT}"`> $*"
 }
+
+ts_echo "${MYNAME}: start"
 
 GITNAME="ytIrTools"
 . ${HOME}/bin/activate-${GITNAME}
-
-ts_echo "> ${MYNAME}: start"
+ts_echo "VIRTUAL_ENV=${VIRTUAL_ENV}"
 
 LOGDIR=${HOME}/tmp
+ts_echo "LOGDIR=${LOGDIR}"
 
 NAME_PIGPIOD="pigpiod"
 PID_PIGPIOD=`pgrep ${NAME_PIGPIOD}`
+ts_echo "PID_PIGPIOD=${PID_PIGPIOD}"
 
 NAME_IRSENDSERVER="IrSendCmdServer"
 LOG_IRSENDSERVER=${LOGDIR}/${NAME_IRSENDSERVER}.log
 PID_IRSENDSERVER=`pgrep -f python.\*${NAME_IRSENDSERVER}.py`
+ts_echo "PID_IRSENDSERVER=${PID_IRSENDSERVER}"
 
 NAME_AUTOAIRCON="AutoAirconServer"
 LOG_AUTOAIRCON=${LOGDIR}/${NAME_AUTOAIRCON}.log
 PID_AUTOAIRCON=`pgrep -f python.\*${NAME_AUTOAIRCON}.py`
+ts_echo "PID_AUTOAIRCON=${PID_AUTOAIRCON}"
 TARGET_TEMP=25
-
 
 if [ X$PID_PIGPIOD = X ]; then
     ts_echo "start pigpiod"
     sudo pigpiod -t 0
-    sleep 3
 fi
 
 while [ `/sbin/ifconfig -a | grep inet | grep -v inet6 | grep -v 127.0.0 | wc -l` -eq 0 ]; do
@@ -41,23 +44,19 @@ while [ `/sbin/ifconfig -a | grep inet | grep -v inet6 | grep -v 127.0.0 | wc -l
 done
 
 if [ X$PID_IRSENDSERVER = X ]; then
-    if [ -x ${HOME}/bin/${NAME_IRSENDSERVER}.py ]; then
-	if [ -f ${LOG_IRSENDSERVER} ]; then
-	    mv -fv ${LOG_IRSENDSERVER} ${LOG_IRSENDSERVER}.1
-	fi
-	ts_echo "start ${NAME_IRSENDSERVER}"
-	${HOME}/bin/${NAME_IRSENDSERVER}.py > ${LOG_IRSENDSERVER} 2>&1 &
+    if [ -f ${LOG_IRSENDSERVER} ]; then
+	mv -fv ${LOG_IRSENDSERVER} ${LOG_IRSENDSERVER}.1
     fi
+    ts_echo "start ${NAME_IRSENDSERVER}"
+    ${NAME_IRSENDSERVER}.py > ${LOG_IRSENDSERVER} 2>&1 &
 fi
 
 if [ X$PID_AUTOAIRCON = X ]; then
-    if [ -x ${HOME}/bin/${NAME_AUTOAIRCON}.py ]; then
-	if [ -f ${LOG_AUTOAIRCON} ]; then
-	    mv -fv ${LOG_AUTOAIRCON} ${LOG_AUTOAIRCON}.1
-	fi
-	ts_echo "start ${NAME_AUTOAIRCON}"
-	${HOME}/bin/${NAME_AUTOAIRCON}.py ${TARGET_TEMP} > ${LOG_AUTOAIRCON} 2>&1 &
+    if [ -f ${LOG_AUTOAIRCON} ]; then
+	mv -fv ${LOG_AUTOAIRCON} ${LOG_AUTOAIRCON}.1
     fi
+    ts_echo "start ${NAME_AUTOAIRCON}"
+    ${NAME_AUTOAIRCON}.py ${TARGET_TEMP} > ${LOG_AUTOAIRCON} 2>&1 &
 fi
 
-ts_echo "< ${MYNAME}: end"
+ts_echo "${MYNAME}: end"
--- setup.sh
+++ setup.sh
@@ -2,80 +2,111 @@
 #
 # (c) 2020 Yoichi Tanibayashi
 #
+GITS="ytMQTT common_python"
+
+CMDS="IrAnalyze.py IrSendCmdServer.py IrSendCmdClient.py AutoAirconServer.py"
+
+BINCMDS="boot.sh ir-analyze ir-send dyson.sh dyson-temp.sh tv-light-level.sh"
+
+
+echo "GITS=${GITS}"
+echo "CMDS=${CMDS}"
+echo "BINCMDS=${BINCMDS}"
+
 MYNAME=`basename $0`
+echo "MYNAME=${MYNAME}"
+
+cd `dirname $0`
 MYDIR=`pwd`
-GITNAME=`basename $MYDIR`
-echo "MYNAME=$MYNAME"
-echo "MYDIR=$MYDIR"
+echo "MYDIR=${MYDIR}"
+
+GITNAME=`basename ${MYDIR}`
 echo "GITNAME=$GITNAME"
 
-BINDIR="${HOME}/bin"
+VENV_DIR=`dirname ${MYDIR}`
+echo "VENV_DIR=${VENV_DIR}"
+
+BINDIR="${VENV_DIR}/bin"
+echo "BINDIR=${BINDIR}"
 if [ ! -d ${BINDIR} ]; then
-    mkdir -pv ${BINDIR}
+    echo "${BINDIR}: no such directory" >&2
+fi
+
+HOMEBIN="${HOME}/bin"
+echo "HOMEBIN=${HOMEBIN}"
+if [ ! -d ${HOMEBIN} ]; then
+    mkdir -pv ${HOMEBIN}
 fi
 
 LOGDIR="${HOME}/tmp"
+echo "LOGDIR=${LOGDIR}"
+if [ ! -d ${LOGDIR} ]; then
+    mkdir -pv ${LOGDIR}
+fi
 
 IRCONF_D="${HOME}/.irconf.d"
+echo "IRCONF_D=${IRCONF_D}"
 
 MQTT_DIR="ytMQTT"
+echo "MQTT_DIR=${MQTT_DIR}"
 
 CRONTAB_FILE="crontab.sample"
+echo "CRONTAB_FILE=${CRONTAB_FILE}"
 
-echo 
 ##### main
-if [ X${VIRTUAL_ENV} = X ]; then
-    echo "You must use venv."
+if [ ! -f ${BINDIR}/activate ]; then
+    echo "${BINDIR}/activate: no such file" >&2
     exit 1
 fi
-VENV_NAME=`basename ${VIRTUAL_ENV}`
-echo "VENV_NAME=$VENV_NAME"
+. ${BINDIR}/activate
 
+echo "VIRTUAL_ENV=${VIRTUAL_ENV}"
+ln -s ${BINDIR}/activate ${HOMEBIN}/activate-${GITNAME}
 
-# make symlink to activate venv
-ln -sfv ${VIRTUAL_ENV}/bin/activate ${BINDIR}/activate-${GITNAME}
+cd ${VIRTUAL_ENV}
+echo "[" `pwd` "]"
 
+for g in ${GITS}; do
+    git clone git@github.com:ytani01/${g}
+done
+
+cd ${MYDIR}
+echo "[" `pwd` "]"
 
 # Python3 packages
 if [ -f requirements.txt ]; then
     pip3 install -r requirements.txt 
 fi
 
+# make symlink to activate venv
+#ln -sfv ${VIRTUAL_ENV}/bin/activate ${BINDIR}/activate-${GITNAME}
 
 # install Node-RED ### T.B.D ###
-
 
 # AutoAircon
 cp -v dot.autoaircon ${HOME}/.autoaircon 
 cp -v dot.autoaircon-param ${HOME}/.autoaircon-param
 
-# MQTT
-echo "${MQTT_DIR} .."
-if [ -d ../${MQTT_DIR} ]; then
-    echo "found."
-else
-    cd ..
-    pwd
-    git clone git@github.com:ytani01/${MQTT_DIR}.git
-fi
-
 # Commands
-cd ${BINDIR}
-echo "[" `pwd` "]"
+for c in ${CMDS}; do
+    ln -sfv ${MYDIR}/$c ${BINDIR}/$c
+done
 
-for f in ir-analyze ir-send IrAnalyze.py IrSendCmdServer.py IrSendCmdClient.py AutoAirconServer.py boot.sh dyson.sh dyson-temp.sh tv-light-level.sh ; do
-    ln -sfv ${MYDIR}/$f ${BINDIR}/$f
+for c in ${BINCMDS}; do
+    ln -sfv ${MYDIR}/$c ${HOMEBIN}/$c
 done
 
 # irconf
 if [ ! -d ${IRCONF_D} ]; then
-    mkdir -p ${IRCON_D}
+    mkdir -pv ${IRCON_D}
 fi
-cd ${IRCONF_D}
-ln -sfv ${MYDIR}/irconf.d/* .
+ln -sfv ${MYDIR}/irconf.d/* ${IRCONF_D}
+
+# activate-exec.sh
+ln -s ${VIRTUAL_ENV}/common_python/activate-exec.sh ${HOMEBIN}
 
 # crontab (auto boot)
-cd ${MYDIR}
-crontab -l ${HOME}/tmp/crontab.bak
-crontab ${CRONTAB_FILE}
+#cd ${MYDIR}
+#crontab -l ${HOME}/tmp/crontab.bak
+#crontab ${CRONTAB_FILE}
 
