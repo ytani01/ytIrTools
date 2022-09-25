31084fafdbd222ec397b6b9f2103fb4f8b6a4d9a
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-09-25 21:43:36 +0900

    UPD: AutoAirconServer.py

--- AutoAirconServer.py
+++ AutoAirconServer.py
@@ -694,7 +694,8 @@ class AutoAirconCmd(Cmd):
         if len(args) == 1:
             return self.RC_OK, self._ttemp
 
-        self._i = 0
+        # self._i = 0
+
         try:
             self._ttemp = float(args[1])
             self._param_cl.send_param({'ttemp': self._ttemp})
